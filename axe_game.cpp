f4af075f5751b89797988cdb1cef8fd5b29ebca6
Manny <emanuel.juracic@hotmail.ca>
2023-01-02 22:41:47 -0500

    Circle is drawn on the screen and can be moved by user. D = right, A = left, W = up, S = down

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,10 +1,17 @@
 #include "raylib.h"
 
+//Window Dimensions
 const int windowWidth = 350;    //setting the window width 
 const int windowHeight = 200;   //setting the window height value
 
 int main()
 {
+    //Circle Dimensions
+    float circleXCoordinate = 175;  //Setting the X coordinates where the Circle will be related to the X-Axis of the window
+    float circleYCoordinate = 100;  //Setting the Y coordinates where the Circle will be related to the Y-Axis of the window
+    float circleRadius = 25;        //Radius of the circle
+
+
     //initializing the window
     InitWindow(windowWidth, windowHeight, "Axe Game");
 
@@ -13,7 +20,40 @@ int main()
     while (WindowShouldClose() == false)
     {
         BeginDrawing(); //drawing on the canvas within the window
-        ClearBackground(RED); //clearing the background to avoid the screen flickering
+        ClearBackground(WHITE); //clearing the background to avoid the screen flickering
+
+        //game logic begins
+
+
+        DrawCircle(circleXCoordinate, circleYCoordinate, circleRadius, BLUE); //first two parameters represent coordinates within the X & Y axis of the screen, third parameter is radius of the circle. Last parameter is color
+        
+        //go right
+        if(IsKeyDown(KEY_D))
+        {
+            circleXCoordinate = circleXCoordinate + 0.1;
+        }
+
+        //go left
+        if(IsKeyDown(KEY_A))
+        {
+            circleXCoordinate = circleXCoordinate - 0.1;
+        }
+
+        //go up
+        if(IsKeyDown(KEY_W))
+        {
+            circleYCoordinate = circleYCoordinate - 0.1;
+        }
+
+        //go down
+        if(IsKeyDown(KEY_S))
+        {
+            circleYCoordinate = circleYCoordinate + 0.1;
+        }
+
+
+        //game logic ends
+
         EndDrawing();   //ending the drawing process
     }
 }
