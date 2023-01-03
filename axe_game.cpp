6e63795a716dbdc7bc7dc7865a714651861708d7
Manny <emanuel.juracic@hotmail.ca>
2023-01-02 23:27:10 -0500

    added code that blocks the circle from moving past the screen.

--- axe_game.cpp
+++ axe_game.cpp
@@ -7,13 +7,15 @@ const int windowHeight = 200;   //setting the window height value
 int main()
 {
     //Circle Dimensions
-    float circleXCoordinate = 175;  //Setting the X coordinates where the Circle will be related to the X-Axis of the window
-    float circleYCoordinate = 100;  //Setting the Y coordinates where the Circle will be related to the Y-Axis of the window
-    float circleRadius = 25;        //Radius of the circle
+    int circleXCoordinate = 175;  //Setting the X coordinates where the Circle will be related to the X-Axis of the window
+    int circleYCoordinate = 100;  //Setting the Y coordinates where the Circle will be related to the Y-Axis of the window
+    int circleRadius = 25;        //Radius of the circle
 
 
     //initializing the window
     InitWindow(windowWidth, windowHeight, "Axe Game");
+
+    SetTargetFPS(60);
 
 //WindowShouldClose returns true if user hits the X button or presses X
 //so this while loop will keep running until the user does either one of the two conditions above and end the game
@@ -27,28 +29,30 @@ int main()
 
         DrawCircle(circleXCoordinate, circleYCoordinate, circleRadius, BLUE); //first two parameters represent coordinates within the X & Y axis of the screen, third parameter is radius of the circle. Last parameter is color
         
+        //the and statements below represent the boundaries that the circle can work within the window. If it tries to overstep, it is blocked from moving in that direction
+        
         //go right
-        if(IsKeyDown(KEY_D))
+        if(IsKeyDown(KEY_D) && circleXCoordinate < windowWidth)
         {
-            circleXCoordinate = circleXCoordinate + 0.1;
+            circleXCoordinate = circleXCoordinate + 5;
         }
 
         //go left
-        if(IsKeyDown(KEY_A))
+        if(IsKeyDown(KEY_A) && circleXCoordinate > 0)
         {
-            circleXCoordinate = circleXCoordinate - 0.1;
+            circleXCoordinate = circleXCoordinate - 5;
         }
 
         //go up
-        if(IsKeyDown(KEY_W))
+        if(IsKeyDown(KEY_W) && circleYCoordinate > 0)
         {
-            circleYCoordinate = circleYCoordinate - 0.1;
+            circleYCoordinate = circleYCoordinate - 5;
         }
 
         //go down
-        if(IsKeyDown(KEY_S))
+        if(IsKeyDown(KEY_S) && circleYCoordinate < windowHeight)
         {
-            circleYCoordinate = circleYCoordinate + 0.1;
+            circleYCoordinate = circleYCoordinate + 5;
         }
 
 
